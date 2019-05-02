9e4b500ba375958ebf84108e110db9d9e48a4f19
long-lehoang <long.bk.khmt@gmail.com>
2019-05-02 11:27:17 +0700

    Upload app

--- music/homepage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.Homepage),
-    path('signup/', views.SignUp),
-    path('login/', views.Login),
-    path('logout/', views.Logout),
-]
