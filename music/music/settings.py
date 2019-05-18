53d2609288e2ca43849e1feb3f838a1b293dcd96
Duc Vo Van <vovanducstudent@gmail.com>
2019-05-18 07:54:31 +0700

    setting run in another computer

--- music/music/settings.py
+++ music/music/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3ai8bo&3nwizo60ebb-(%d=bf@m3^d%94fx7frp*bclrdbp$kh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
